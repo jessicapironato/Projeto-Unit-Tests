1730775a202637ffbd17011170327ecf9d033bca
Jéssica <jessicapmaximo@gmail.com>
2022-11-23 19:28:29 -0300

    6 testes ok, falta 1

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -34,17 +34,13 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.', () => {
     expect(circle()).toEqual(undefined);
   });
-
-
   it('Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à circunferência correta para um círculo de raio 2.', () => {
-    
+    expect(circle(2)['circumference']).toEqual(12.56);    
   });
   it('Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à área correta para um círculo de raio 3.', () => {
-    expect(Object.keys(circle(8)).length).toBe(3);
+    expect(circle(3)['area']).toBeCloseTo(28.26);    
   });
-
-  it('Teste se a função retorna, em um objeto, os dados corretos de um círculo de raio 3.', () => {
-    
+  it('Teste se a função retorna, em um objeto, os dados corretos de um círculo de raio 3.', () => {    
   });
 });
 
